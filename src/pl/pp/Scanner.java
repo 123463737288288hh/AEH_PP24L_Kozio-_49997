7a8f308fbc6b9130ef33f2f7e6d4ca32858e401d
MalwinaKoziol <malwina.koziol04@wp.pl>
2024-03-18 23:43:44 +0100

    lab3

---
+++ src/pl/pp/Scanner.java
@@ -0,0 +1,16 @@
+package pl.pp;
+
+import java.io.InputStream;
+
+public class Scanner {
+    public Scanner(InputStream in) {
+    }
+
+    public int nextInt() {
+        return 0;
+    }
+
+    public void close() {
+    }
+}
+
